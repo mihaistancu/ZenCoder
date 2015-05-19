6c5b64359a5ce66e9d4bf4af7a878550d1e10f98
mihaistancu <stancu_meehigh@yahoo.com>
2015-05-19 11:43:34 +0300

    Added unit tests project

---
+++ Hangman/Hangman.Tests/GameTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hangman.Tests
+{
+    [TestClass]
+    public class GameTests
+    {
+        [TestMethod]
+        public void GameCanBeCreated()
+        {
+            var game = new Game("word");
+        }
+    }
+}
