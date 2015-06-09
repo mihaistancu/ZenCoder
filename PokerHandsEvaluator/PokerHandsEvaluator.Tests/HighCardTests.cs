f6410768a56ccf8220cab715e1d0a21992b35fc4
mihaistancu <stancu_meehigh@yahoo.com>
2015-06-10 02:17:48 +0300

    Added new high card test case

--- PokerHandsEvaluator/PokerHandsEvaluator.Tests/HighCardTests.cs
+++ PokerHandsEvaluator/PokerHandsEvaluator.Tests/HighCardTests.cs
@@ -11,6 +11,7 @@ namespace PokerHandsEvaluator
         [TestCase("3c 5s 6d 8h 0h", "9h 3s 5h 6c 8d")]
         [TestCase("9h 3c 5s 6d 8h", "3s 5h 8h 6c 4d")]
         [TestCase("3c 8h 5s 6d 4h", "3s 5h 7h 6c 2d")]
+        [TestCase("Ah 3c 5s 6d Kh", "As 5h 6c 8d 4h")]
         public void HighestRankWins(string firstHand, string secondHand)
         {
             AssertFirstHandWins(firstHand, secondHand);
--- PokerHandsEvaluator/PokerHandsEvaluator/PokerHandsEvaluator.cs
+++ PokerHandsEvaluator/PokerHandsEvaluator/PokerHandsEvaluator.cs
@@ -1,12 +1,29 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 
 namespace PokerHandsEvaluator
 {
     public class PokerHandsEvaluator
     {
         public int Compare(Card[] firstHand, Card[] secondHand)
-        {   
-            return firstHand.Max(card => card.Rank) - secondHand.Max(card => card.Rank);
+        {
+            SortDescending(firstHand);
+            SortDescending(secondHand);
+
+            var results = firstHand.Zip(secondHand, Compare);
+
+            return results.FirstOrDefault(result => result != 0);
+        }
+
+        private void SortDescending(Card[] hand)
+        {
+            Array.Sort(hand, Compare);
+            Array.Reverse(hand);
+        }
+
+        private int Compare(Card a, Card b)
+        {
+            return Math.Sign(a.Rank - b.Rank);
         }
     }
 }
