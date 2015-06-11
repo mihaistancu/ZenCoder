6cc141862688f2ca5bf5fced07873f243602df11
mihaistancu <stancu_meehigh@yahoo.com>
2015-06-11 15:26:15 +0300

    Fixed naming for one unit test

--- PokerHandsEvaluator/PokerHandsEvaluator.Tests/TwoPairsTests.cs
+++ PokerHandsEvaluator/PokerHandsEvaluator.Tests/TwoPairsTests.cs
@@ -5,7 +5,7 @@ namespace PokerHandsEvaluator
     public class TwoPairsTests: BaseTests
     {
         [TestCase("As Ac Qs Qd 8h", "Qc Qh Kc Kh 8d")]
-        public void HighestTwoPairWins(string firstHand, string secondHand)
+        public void HighestTwoPairsWins(string firstHand, string secondHand)
         {
             AssertFirstHandWins(firstHand, secondHand);
         }
