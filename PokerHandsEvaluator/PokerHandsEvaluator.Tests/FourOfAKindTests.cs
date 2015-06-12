1023e130f2c700e8d2fe6bef07863a841c947eca
mihaistancu <stancu_meehigh@yahoo.com>
2015-06-12 08:58:23 +0300

    Added four of a kind

---
+++ PokerHandsEvaluator/PokerHandsEvaluator.Tests/FourOfAKindTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Poker
+{
+    public class FourOfAKindTests: BaseTests
+    {
+        [TestCase("As Ac As Qd Ah", "Kc Kh Qc Kh Kd")]
+        public void HighestFourOfAKindWins(string firstHand, string secondHand)
+        {
+            AssertFirstHandWins(firstHand, secondHand);
+        }
+
+        [TestCase("6s 3c 6s 6d 6h", "Ac Ah Kc Kh Ad")]
+        public void FourOfAKindBeatsFullHouse(string firstHand, string secondHand)
+        {
+            AssertFirstHandWins(firstHand, secondHand);
+        }
+    }
+}
--- PokerHandsEvaluator/PokerHandsEvaluator/Hands/Evaluator.cs
+++ PokerHandsEvaluator/PokerHandsEvaluator/Hands/Evaluator.cs
@@ -5,32 +5,25 @@ namespace Poker.Hands
 {
     public class Evaluator
     {
-        private readonly IHand highCard = new HighCard();
-        private readonly IHand onePair = new RankPattern(new[] {2, 1, 1, 1});
-        private readonly IHand twoPairs = new RankPattern(new[] {2, 2, 1});
-        private readonly IHand threeOfAKind = new RankPattern(new[] {3, 1, 1});
-        private readonly IHand straight = new Straight();
-        private readonly IHand flush = new Flush();
-        private readonly IHand fullHouse = new RankPattern(new[] {2, 3});
-        
         public int Compare(Card[] firstHand, Card[] secondHand)
         {
-            return GetComparers()
-                .Select(comparer => comparer.Compare(firstHand, secondHand))
+            return GetHandCategories()
+                .Select(hand => hand.Compare(firstHand, secondHand))
                 .FirstOrDefault(result => result != 0);
         }
         
-        private IEnumerable<IHand> GetComparers()
+        private IEnumerable<IHand> GetHandCategories()
         {
-            return new []
+            return new IHand[]
             {
-                fullHouse,
-                flush,
-                straight,
-                threeOfAKind,
-                twoPairs,
-                onePair,
-                highCard
+                new RankPattern(new[] {4, 1}),
+                new RankPattern(new[] {2, 3}),
+                new Flush(),
+                new Straight(),
+                new RankPattern(new[] {3, 1, 1}),
+                new RankPattern(new[] {2, 2, 1}),
+                new RankPattern(new[] {2, 1, 1, 1}),
+                new HighCard()
             };
         }  
     }
