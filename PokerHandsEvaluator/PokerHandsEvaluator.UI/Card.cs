54c1cda0673c5f2b72bec6d7948c06c094a4c8df
mihaistancu <stancu_meehigh@yahoo.com>
2015-06-12 16:17:12 +0300

    Added logic for Card in UI

--- PokerHandsEvaluator/PokerHandsEvaluator.UI/Card.cs
+++ PokerHandsEvaluator/PokerHandsEvaluator.UI/Card.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Poker;
+using PokerHandsEvaluator.UI.Properties;
 
 namespace PokerHandsEvaluator.UI
 {
@@ -15,6 +12,84 @@ namespace PokerHandsEvaluator.UI
         public Card()
         {
             InitializeComponent();
+        }
+
+        private Rank rank;
+
+        public Rank Rank
+        {
+            get { return rank; }
+            set
+            {
+                rank = value;
+                UpdateImage();
+            }
+        }
+
+        private Suit suit;
+
+        public Suit Suit
+        {
+            get { return suit; }
+            set
+            {
+                suit = value;
+                UpdateImage();
+            }
+        }
+
+        private readonly Dictionary<Rank, string> rankMap = new Dictionary<Rank, string>
+        {
+            {Rank.Ace, "ace"}, {Rank.Two, "2"}, {Rank.Three, "3"}, {Rank.Four, "4"}, {Rank.Five, "5"},
+            {Rank.Six, "6"}, {Rank.Seven, "7"}, {Rank.Eight, "8"}, {Rank.Nine, "9"}, {Rank.Ten, "10"},
+            {Rank.Jack, "jack"}, {Rank.Queen, "queen"}, {Rank.King, "king"}
+        };
+
+        private readonly Dictionary<Suit, string> suitMap = new Dictionary<Suit, string>
+        {
+            {Suit.Clubs, "clubs"},
+            {Suit.Diamonds, "diamonds"},
+            {Suit.Hearts, "hearts"},
+            {Suit.Spades, "spades"},
+        };
+
+        private void UpdateImage()
+        {
+            var imageName = string.Format("{0}_of_{1}", rankMap[Rank], suitMap[Suit]);
+            BackgroundImage = (Image)Resources.ResourceManager.GetObject(imageName);
+        }
+
+        bool dragging;
+        Point dragStart = Point.Empty;
+
+        protected override void OnMouseDown(MouseEventArgs mouseEventArgs)
+        {
+            base.OnMouseDown(mouseEventArgs);
+
+            BringToFront();
+
+            dragging = true;
+            dragStart = new Point(mouseEventArgs.X, mouseEventArgs.Y);
+            Capture = true;
+        }
+
+        protected override void OnMouseUp(MouseEventArgs mouseEventArgs)
+        {
+            base.OnMouseUp(mouseEventArgs);
+
+            dragging = false;
+            Capture = false;
+        }
+
+        protected override void OnMouseMove(MouseEventArgs mouseEventArgs)
+        {
+            base.OnMouseMove(mouseEventArgs);
+
+            if (dragging)
+            {
+                Left = Math.Max(0, mouseEventArgs.X + Left - dragStart.X);
+                Top = Math.Max(0, mouseEventArgs.Y + Top - dragStart.Y);
+            }
         }
     }
 }
--- PokerHandsEvaluator/PokerHandsEvaluator.UI/Main.Designer.cs
+++ PokerHandsEvaluator/PokerHandsEvaluator.UI/Main.Designer.cs
@@ -28,13 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.card2 = new PokerHandsEvaluator.UI.Card();
+            this.card1 = new PokerHandsEvaluator.UI.Card();
             this.SuspendLayout();
+            // 
+            // card2
+            // 
+            this.card2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("card2.BackgroundImage")));
+            this.card2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.card2.Location = new System.Drawing.Point(279, 357);
+            this.card2.Name = "card2";
+            this.card2.Rank = Poker.Rank.Two;
+            this.card2.Size = new System.Drawing.Size(90, 130);
+            this.card2.Suit = Poker.Suit.Clubs;
+            this.card2.TabIndex = 1;
+            // 
+            // card1
+            // 
+            this.card1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("card1.BackgroundImage")));
+            this.card1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.card1.Location = new System.Drawing.Point(127, 399);
+            this.card1.Name = "card1";
+            this.card1.Rank = Poker.Rank.Eight;
+            this.card1.Size = new System.Drawing.Size(90, 130);
+            this.card1.Suit = Poker.Suit.Hearts;
+            this.card1.TabIndex = 0;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1003, 737);
+            this.Controls.Add(this.card2);
+            this.Controls.Add(this.card1);
             this.Name = "Main";
             this.Text = "Poker Hands Evaluator";
             this.ResumeLayout(false);
@@ -42,6 +69,9 @@
         }
 
         #endregion
+
+        private Card card1;
+        private Card card2;
     }
 }
 
